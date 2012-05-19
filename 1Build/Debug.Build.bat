434dd0d90ce7070e2ee6157b3ba3ce5a1117620d
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-19 11:21:09 +0800

    Modified Build Script Execution

--- 1Build/Debug.Build.bat
+++ 1Build/Debug.Build.bat
@@ -1,5 +1,5 @@
-..\Tools\NAnt\NAnt.exe -buildfile:Scripts\Debug.build > debug.build.log
+..\Tools\NAnt\NAnt.exe -buildfile:Scripts\Debug.build > Logs\debug.build.log
 
 @echo **** done.  check build.log for errors ****
 pause
-start notepad.exe debug.build.log
+start notepad.exe Logs\debug.build.log
--- 1Build/Release.Build.bat
+++ 1Build/Release.Build.bat
@@ -1,5 +1,5 @@
-..\Tools\NAnt\NAnt.exe -buildfile:Scripts\Release.build > release.build.log
+..\Tools\NAnt\NAnt.exe -buildfile:Scripts\Release.build > Logs\release.build.log
 
 @echo **** done.  check build.log for errors ****
 pause
-start notepad.exe release.build.log
+start notepad.exe Logs\release.build.log

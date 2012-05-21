849e2cf7c37be3197f5e89629b0084fc1b46041b
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-21 17:06:32 +0800

    Added WCF Contract for DataSession

--- Meek.Data/IDataSession.cs
+++ Meek.Data/IDataSession.cs
@@ -1,13 +1,19 @@
-﻿namespace Meek.Data
+﻿using System.ServiceModel;
+
+namespace Meek.Data
 {
+    [ServiceContract]
     public interface IDataSession
     {
         IDataProvider Provider { get; set; }
 
+        [OperationContract(Name = "BeginTransaction")]
         void BeginTransaction();
 
+        [OperationContract(Name = "CommitTransaction")]
         void CommitTransaction();
 
+        [OperationContract(Name = "RollbackTransaction")]
         void RollbackTransaction();
 
     }

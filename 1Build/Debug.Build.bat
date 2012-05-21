38c4b9fdf712aeb1c6b4baf129bf94be0f6e34bb
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-21 16:58:13 +0800

    Added Logs/.gitignore

--- 1Build/Debug.Build.bat
+++ 1Build/Debug.Build.bat
@@ -1,4 +1,4 @@
-..\Tools\NAnt\NAnt.exe -buildfile:Scripts\Debug.build > Logs\debug.build.log
+..\Tools\NAnt\NAnt.exe -buildfile:Scripts\Debug.build > Logs\release.build.log
 
 @echo **** done.  check build.log for errors ****
 pause

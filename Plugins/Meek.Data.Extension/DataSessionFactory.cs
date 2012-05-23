3c1fbc1efb129e319a8d96125a2aea472ee6598a
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-23 19:22:37 +0800

    Added DataSessionFactory.cs

---
+++ Plugins/Meek.Data.Extension/DataSessionFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Meek.Data.Extension
+{
+    public class DataSessionFactory : IDataSessionFactory
+    {
+        public TDataSession CreateDataSession<TDataSession>()
+           where TDataSession : IDataSession
+        {
+            return default(TDataSession);
+        }
+
+        public TDataSession CreateDataSession<TDataSession>(TDataSession defaultSession)
+            where TDataSession : IDataSession
+        {
+            return defaultSession;
+        }
+    }
+}

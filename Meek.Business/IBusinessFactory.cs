c93da1cd4e6de722a9e3907c42f8c1304ee4d949
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-26 00:54:52 +0800

    Added IBusinessFactory.cs

---
+++ Meek.Business/IBusinessFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Meek.Business
+{
+    public interface IBusinessFactory
+    {
+        TLogic CreateBusinessLogic<TLogic>()
+            where TLogic : ILogic;
+
+        TLogic CreateBusinessLogic<TLogic>(TLogic defaultLogic)
+            where TLogic : ILogic;
+    }
+}

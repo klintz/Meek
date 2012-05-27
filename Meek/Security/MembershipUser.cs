11d48007d293a62236fab23b43d13e1cf1387ded
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-27 23:23:21 +0800

    Modified MembershipUser.cs

--- Meek/Security/MembershipUser.cs
+++ Meek/Security/MembershipUser.cs
@@ -7,31 +7,31 @@ namespace Meek.Security
 {
     public class MembershipUser : IMembershipUser
     {
-        public string Username { get; protected set; }
+        public virtual string Username { get; protected set; }
 
-        public string Email { get; set; }
+        public virtual string Email { get; set; }
 
-        public DateTime CreationDate { get; protected set; }
+        public virtual DateTime CreationDate { get; protected set; }
 
-        public string Comment { get; set; }
+        public virtual string Comment { get; set; }
 
-        public bool IsOnline { get; protected set; }
+        public virtual bool IsOnline { get; protected set; }
 
-        public bool IsLockedOut { get; protected set; }
+        public virtual bool IsLockedOut { get; protected set; }
 
-        public bool IsApproved { get; set; }
+        public virtual bool IsApproved { get; set; }
 
-        public DateTime LastActivityDate { get; set; }
+        public virtual DateTime LastActivityDate { get; set; }
 
-        public DateTime LastLockoutDate { get; protected set; }
+        public virtual DateTime LastLockoutDate { get; protected set; }
 
-        public DateTime LastLoginDate { get; set; }
+        public virtual DateTime LastLoginDate { get; set; }
 
-        public DateTime LastPasswordChangedDate { get; protected set; }
+        public virtual DateTime LastPasswordChangedDate { get; protected set; }
 
-        public string PasswordQuestion { get; protected set; }
+        public virtual string PasswordQuestion { get; protected set; }
 
-        public string ProviderName { get; protected set; }
+        public virtual string ProviderName { get; protected set; }
 
         public object ProviderUserKey { get; protected set; }
     }

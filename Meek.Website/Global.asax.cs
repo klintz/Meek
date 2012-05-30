fa02ebf33bb9b3b37dae23edc7e11f8e74384470
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-30 15:25:19 +0800

    Modified Routes

--- Meek.Website/Global.asax.cs
+++ Meek.Website/Global.asax.cs
@@ -21,11 +21,14 @@ namespace Meek.Website
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
+            routes.MapRoute("Index", "", new {controller = "Home", action = "Index"});
+            
+            
+            //routes.MapRoute(
+            //    "Default", // Route name
+            //    "{controller}/{action}/{id}", // URL with parameters
+            //    new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            //);
 
         }
 

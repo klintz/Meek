c0a44fa46e13533c4ab3947acef8f7815dd03380
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2012-05-31 15:33:12 +0800

    added ExtendedRazorViewEngine.cs

---
+++ Meek.Web.Mvc/ExtendedRazorViewEngine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Meek.Web.Mvc
+{
+    public class ExtendedRazorViewEngine : RazorViewEngine
+    {
+
+        #region Constructor
+        public ExtendedRazorViewEngine()
+        {
+            //Areas
+            AreaViewLocationFormats = new[] {
+                "~/Areas/{2}/{1}/{0}.cshtml",
+                "~/Areas/{2}/{1}/{0}.vbhtml",
+                "~/Areas/{2}/Shared/{0}.cshtml",
+                "~/Areas/{2}/Shared/{0}.vbhtml"
+            };
+
+            AreaMasterLocationFormats = new[] {
+                "~/Areas/{2}/{1}/{0}.cshtml",
+                "~/Areas/{2}/{1}/{0}.vbhtml",
+                "~/Areas/{2}/Shared/{0}.cshtml",
+                "~/Areas/{2}/Shared/{0}.vbhtml"
+            };
+
+            AreaPartialViewLocationFormats = new[] {
+                "~/Areas/{2}/{1}/{0}.cshtml",
+                "~/Areas/{2}/{1}/{0}.vbhtml",
+                "~/Areas/{2}/Shared/{0}.cshtml",
+                "~/Areas/{2}/Shared/{0}.vbhtml"
+            };
+
+            //Root
+            ViewLocationFormats = new[] {
+                "~/{0}.cshtml",
+                "~/{0}.vbhtml",
+                "~/{1}/{0}.cshtml",
+                "~/{1}/{0}.vbhtml"
+            };
+
+            PartialViewLocationFormats = new[]{
+                "~/{0}.cshtml",
+                "~/{0}.vbhtml",
+                "~/{1}/{0}.cshtml",
+                "~/{1}/{0}.vbhtml",
+                "~/Controls/{0}.cshtml",
+                "~/Controls/{0}.vbhtml"
+            };
+
+            MasterLocationFormats = new[] {
+                "~/{0}.cshtml",
+                "~/{0}.vbhtml",
+                "~/{1}/{0}.cshtml",
+                "~/{1}/{0}.vbhtml",
+                "~/Shared/{0}.cshtml",
+                "~/Shared/{0}.vbhtml"
+            };
+        }
+        #endregion
+
+        public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            if (controllerContext == null)
+                throw new ArgumentNullException("controllerContext");
+
+            if (string.IsNullOrEmpty(viewName))
+                throw new ArgumentException("Value is required.", "viewName");
+
+            var controllerName = controllerContext.RouteData.GetRequiredString("controller");
+
+            return base.FindView(controllerContext, viewName, masterName, useCache);
+        }
+
+        public override ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            if (controllerContext == null)
+                throw new ArgumentNullException("controllerContext");
+
+            if (string.IsNullOrEmpty(partialViewName))
+                throw new ArgumentException("Value is required.", "partialViewName");
+
+            return base.FindPartialView(controllerContext, partialViewName, useCache);
+        }
+    }
+}
---
+++ Meek.Web.Mvc/Schema/ViewItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Meek.Web.Mvc.Schema
+{
+    [Serializable]
+    [XmlRoot("ViewItem",
+        Namespace = "http://www.meek-framework.com/",
+        IsNullable = true)]
+    public class ViewItem
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("File")]
+        public string File { get; set; }
+
+        [XmlElement("Themed")]
+        public bool Themed { get; set; }
+    }
+}
